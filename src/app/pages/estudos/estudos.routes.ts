df890b7918e9a706428efb234f0468ee05b0e53f
Rafael Mih <rafaelmih@hotmail.com>
2025-03-19 19:55:56 -0300

    estudos harmonicos

--- src/app/pages/estudos/estudos.routes.ts
+++ src/app/pages/estudos/estudos.routes.ts
@@ -1,17 +1,31 @@
-import { Routes } from '@angular/router';
-
+import { Routes } from '@angular/router'
 
 // pages
-import { EstudoCampoHarmonicoComponent } from './campoharmonico/estudo-campoharmonico.component';
+import { EstudoCampoHarmonicoComponent } from './campoharmonico/estudo-campoharmonico.component'
+import { FuncoesHarmonicasComponent } from './funcoesharmonicas/funcoesharmonicas.component'
+import { CadenciasComponent } from './cadenciasharmonicas/cadencias.component'
+import { ProgressoesHarmonicasComponent } from './progressoesharmonicas/progressoesharmonicas.component'
 
 export const EstudosRoutes: Routes = [
-  {
-    path: '',
-    children: [
-      {
-        path: 'campoharmonico',
-        component: EstudoCampoHarmonicoComponent,
-      }
-    ],
-  },
-];
+    {
+        path: '',
+        children: [
+            {
+                path: 'campoharmonico',
+                component: EstudoCampoHarmonicoComponent,
+            },
+            {
+                path: 'funcoesharmonicas',
+                component: FuncoesHarmonicasComponent,
+            },
+            {
+                path: 'cadenciasharmonicas',
+                component: CadenciasComponent,
+            },
+            {
+                path: 'progressoesharmonicas',
+                component: ProgressoesHarmonicasComponent,
+            },
+        ],
+    },
+]
